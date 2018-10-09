9f45525f66c27d5c4784df9e3e54f6724487c0ab
LuciaAndrea <43391254+LuciaAndrea@users.noreply.github.com>
2018-10-09 23:14:15 +0200

    creation and color

--- sketch.js
+++ sketch.js
@@ -1,11 +1,45 @@
 function preload(){
-  // put preload code here
+
 }
 
+var colorList = ['#f59200',
+                 '#E80C7A',
+                 '#5D00FF',
+                 '#0080F5'];
+
 function setup() {
-  // put setup code here
+
+
+    createCanvas(windowWidth, windowHeight);
+background(255, 157, 154);
+frameRate(200)
 }
 
 function draw() {
-  // put drawing code here
+
+
+  var x = random() * width;
+    var y = random() * height;
+    var r = random() * 110;
+
+
+
+          if(random()<0.25){
+             fill(colorList[0]);
+          }
+          else if (random()>0.25 && random()<0.5) {
+            fill(colorList[1]);
+          }
+          else if (random()>0.5 && random()<0.75) {
+            fill(colorList[2]);
+          }
+          else  {
+            fill(colorList[3]);
+          }
+
+    noStroke();
+
+    ellipse(x, y, r);
+
+
 }
